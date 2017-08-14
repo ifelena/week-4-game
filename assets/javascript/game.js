8078832aed4723400c8576407140aa503f51cc62
Ife Ramsey-Majors <junglemunchkin@Ifes-MacBook-Air.local>
2017-08-13 21:31:10 -0600

    I uploaded the folder

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+
+/*Player is shown random # at start of game */
+function randomIntFromInterval(15,42)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+Math.floor((Math.random() * 10) + 1);
+
+/*  */
+
+
+$('#wins').html('winss++')
+$('#loss').html('loss++')
